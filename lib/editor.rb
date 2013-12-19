f2643baf6dceaad06bed094991d7e83864e8f3f9
Sam Auciello <olleicua@gmail.com>
2013-12-19 18:52:44 -0500

    adding skeleton editor class

---
+++ lib/editor.rb
@@ -0,0 +1,15 @@
+# Renders a curses based editor for a ActiveRecord record.
+
+class Editor
+  attr_accessor :record
+  def initialize record
+    @record = record
+  end
+  
+  def display
+    # TODO: implement me
+  end
+  
+  # TODO: implement me
+  
+end

2290d56baed8464ef1785383280d68acb526f1cd
Sam Auciello <olleicua@gmail.com>
2013-12-22 13:20:11 -0500

    cleaning up the import module

--- lib/importer.rb
+++ lib/importer.rb
@@ -4,7 +4,7 @@ class Importer; end
 
 class << Importer
   
-  SUPPORTED_EXTENTIONS = %w( mp3 wav mp4 )
+  SUPPORTED_EXTENTIONS = %w( .mp3 .wav .mp4 )
   
   def run directory
     files(directory).each do |file|
@@ -17,11 +17,11 @@ class << Importer
       path = File.join(directory, file)
       if file =~ /^\./
         nil
-      elsif SUPPORTED_EXTENTIONS.include? file.split('.').last.downcase
-        path
       elsif File.directory? file
         files path
-      elseX2
+      elsif SUPPORTED_EXTENTIONS.include? File.extname(file)
+        path
+      else
         nil
       end
     end.flatten.compact
@@ -30,20 +30,12 @@ class << Importer
   
   def import path
     file = FileImport.new path
-    puts "Importing #{file} .."
-    track = Track.create(:file => File.basename(path))
-    
-    {
-      :track_number => file.metaData['Track name/Position'],
-      :original => nil,
-      :bytes => file.fileSize,
-      :seconds => file.duration
-    }.each_pair do |attr, default|
-      print "track.#{attr} (#{default}): "
-      value = gets.strip
-      value = default if value.empty?
-      track.update_attribute(attr, value)
-    end
+    puts "Importing #{path} .."
+    track = Track.create(:file => path,
+                         :track_number => file.metaData['Track name/Position'],
+                         :original => nil,
+                         :bytes => file.fileSize,
+                         :seconds => file.duration)
     
     # TODO: implement me
   end
